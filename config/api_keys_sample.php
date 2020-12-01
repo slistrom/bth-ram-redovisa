ca56cb4b9166aacc0786c0744985b43644cc1bb3
Stefan <stefan@listrom.se>
2020-12-01 15:20:46 +0100

    Installed remserver and removed IP and Weather functionality to a component

--- config/api_keys_sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Config file.
- * Remove "_sample" from the name of this file and input your own api keys for the required services to work.
- */
-return [
-    "ipstack" => "xyz",
-    "openweather" => "xyz",
-];
